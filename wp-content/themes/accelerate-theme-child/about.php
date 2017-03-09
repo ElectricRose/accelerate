53b780b85ca0b8220c95f206a05bc9f1fed3cb19
Razorcake <sp00kybug@yahoo.ca>
2017-03-09 14:39:06 -0800

    updated child

--- wp-content/themes/accelerate-theme-child/about.php
+++ wp-content/themes/accelerate-theme-child/about.php
@@ -16,14 +16,30 @@ get_header(); ?>
 			<div class='about-hero'>
 				<?php the_content(); ?>
 		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
-	</section><!-- .home-page -->
+		<div class="about-container">
+			<h4>Our Services</h4>
+			<p>We take pride in our clients and the content we create for them.<br>
+				Here's a brief overview of our offered services.</p>
+				<div class="image1">
+				</div>
+			<div class="service1">
+			</div>
+			<div class="image2">
+			</div>
+			<div class="service2">
+			</div>
+			<div class="image3">
+			</div>
+			<div class="service3">
+			</div>
+			<div class="image4">
+			</div>
+			<div class="service4">
+			</div>
+	</div>
+</div>
+</div>
+</section>
 
-<section class=""
-
-
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
 
 <?php get_footer(); ?>
