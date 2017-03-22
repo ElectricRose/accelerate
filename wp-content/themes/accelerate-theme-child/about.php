3053227ecba8fabf941eb80873402c09cb8fc822
Razorcake <sp00kybug@yahoo.ca>
2017-03-22 16:45:27 -0700

    made changes to about page, think it might finally be working better. also trying to get other images to show on front-page.php.

--- wp-content/themes/accelerate-theme-child/about.php
+++ wp-content/themes/accelerate-theme-child/about.php
@@ -12,36 +12,50 @@
 get_header(); ?>
 <section class="about-page">
 	<div class="about-content" role="main">
+		<div class="site-content">
 
-		<?php query_posts('post_type=services'); ?>
-		<?php while ( have_posts() ) : the_post();
-			$size = "full";
-			$our_services = get_field('our_services');
-			$image1 = get_field('image1');
-			$service1 = get_field('service1');
-			$service2 = get_field('service2');
-			$image2 = get_field('image2');
-			$image3 = get_field('image3');
-			$service3 = get_field('service3');
-			$service4 = get_field('service4');
-			$image4 = get_field('image4');
-			$work_with_us = get_field('work_with_us'); ?>
+			<ul class="services">
+				<?php query_posts('post_type=services'); ?>
+				<?php while ( have_posts() ) : the_post();
 
-			<article class="services">
+				$our_services = get_field('our_services');
+				$image_10 = get_field('image_10');
+				$image_11 = get_field('image_11');
+				$image_12 = get_field('image_12');
+				$image_13 = get_field('image_13');
+				$size = "full"; ?>
+
+			<li class="individual-services">
 						<h2><?php the_title(); ?></h2>
-						<h5><?php echo $our_services; ?></h5>
-						<h5><?php echo $service1; ?></h5>
-						<h5><?php echo $service2; ?></h5>
-						<h5><?php echo $service3; ?></h5>
-						<h5><?php echo $service4; ?></h5>
-						<h5><?php echo $work_with_us; ?></h5>
-					</article>
-
-		<div>
-			<?php echo wp_get_attachment_image($image1, $image2, $image3, $image4, $size ); ?>
+						<h5>We take pride in our clients and the content we create for them.<br> Here's a brief overview of our offered services</h5>
+						<figure>
+							<?php echo wp_get_attachment_image($image_10, $size); ?>
+							<h5>Content Strategy</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ante felis, blandit ac orci quis, porttitor mollis ex. Nunc a nunc elit. Etiam id mauris porta, tempus justo a, mollis eros. Donec eget tristique dui. Sed sed vestibulum est.</p>
+						</figure>
+						<figure>
+							<?php echo wp_get_attachment_image($image_11, $size); ?>
+							<h5>Influencer Mapping</h5>
+							<p>Nam id sem maximus, pulvinar neque quis, elementum nulla. In lacinia semper ultricies. Sed venenatis diam a tincidunt interdum. Quisque non maximus lorem, et molestie purus. Cras pharetra mi nulla, eget ornare neque mollis quis.</p>
+						</figure>
+						<figure>
+							<?php echo wp_get_attachment_image($image_12, $size); ?>
+							<h5>Social Media Strategy</h5>
+							<p>Nulla nec sapien in risus tempus cursus. Morbi bibendum nunc sed diam pellentesque bibendum in at purus. Curabitur luctus sapien in neque rhoncus ullamcorper. Phasellus aliquet in enim eu consectetur. Cras non lectus nec dolor faucibus volutpat.</p>
+						</figure>
+						<figure>
+							<?php echo wp_get_attachment_image($image_13, $size); ?>
+							<h5>Design & Development</h5>
+							<p>In non odio fermentum, sodales sem interdum, posuere dolor. Nullam eu rutrum purus. Sed at ultrices ipsum. Curabitur ut finibus diam, in placerat augue. Sed urna massa, lacinia eget mollis et, volutpat at ipsum. Nulla ac consequat magna, eu vulputate ipsum.</p>
+						</figure>
+						<h5>Interested in working with us?</h5>
+			</li>
+				<?php endwhile;
+	  		wp_reset_query(); ?>
+			</ul>
 		</div>
-</div>
-</section>
+	</div>
+	</section>
 
 
 <?php get_footer(); ?>
