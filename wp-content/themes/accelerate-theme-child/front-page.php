429753874ff9fe616992a37d7013b754bcdeaa9b
Razorcake <sp00kybug@yahoo.ca>
2017-03-22 18:27:23 -0700

    got images to show on about.php and front-page.php, needed to use image id instead of url under the custom post types.

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -34,26 +34,15 @@ get_header(); ?>
 			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
 				<?php while ( have_posts() ) : the_post();
 					$image_1 = get_field('image_1');
-					$image_4 = get_field('image_4');
-					$image_5 = get_field('image_5');
 					$size = "medium";
 				?>
 				<li class="individual-featured-work">
 						<figure>
 							<?php echo wp_get_attachment_image($image_1, $size); ?>
-							<h5>MailChimp Campaign</h5>
 						</figure>
-						<figure>
-							<?php echo wp_get_attachment_image($image_4, $size); ?>
-							<h5>Cloroflot Advertising</h5>
-						</figure>
-						<figure>
-							<?php echo wp_get_attachment_image($image_5, $size); ?>
-							<h5>Zurb Foundation Project</h5>
-						</figure>
-					<h3><a href="<?php the_permalink(); ?>" <?php the_title(); ?></a></h3>
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 				</li>
-				<?php endwhile; ?>
+			<?php endwhile; ?>
 				<?php wp_reset_query(); ?>
 			</ul>
 
