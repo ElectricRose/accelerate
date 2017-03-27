7b752f3ca3639bf5e5c4cccd0a209ace666e5ffa
Razorcake <sp00kybug@yahoo.ca>
2017-03-27 13:05:34 -0700

    working on 404 page

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -11,9 +11,10 @@
 
 <section class="404">
   <div class="404-page">
-    <h1>OMG! Why are you here?!<h1>
-      <p>Silly human, this is not the right page..</p>
+    <div class="cat-404">
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cat1.png" alt="Surprised Cat" style="width:1200px;height:898px;">
- 
+      <h1>OMG! Why are you here?!<h1>
+      <p>Silly human, this is not the right page..</p>
+    </div>
   </div>
   </section>
